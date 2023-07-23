e7dd68d636bb1ba5f78575195e509aa6465bf674
codeusser <vladnistor843@gmail.com>
2023-07-23 21:26:15 +0300

    login form stilizat

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -2,15 +2,31 @@ import React from 'react';
 import './login.scss';
 
 export default function Login(props) {
+    function check_sign_in(e){
+        e.preventDefault(); 
+
+    }
     return (
         <>
-            <div className='login-wrapper'>
-                <p>Admin Login</p>
+            <div className='center'>
                 <form>
-                    <label for="fname">First name:</label><br/>
-                    <input type="text" id="fname" name="fname" value="John"/><br/>
-                    <label for="lname">Last name:</label><br/>
-                    <input type="text" id="lname" name="lname" value="Doe"/>
+                    <h1>Admin Login</h1>
+                    <div className="txt_field">
+                        <input  type="text" id="fname" name="fname" required/>
+                        <span></span>
+                        <label for="fname">First name:</label>
+                    </div>
+                    <div className="txt_field1">
+                        <input  type="text" id="lname" name="lname" required/>
+                        <span></span>
+                        <label for="lname">Last name:</label>
+                    </div>
+                    <div className="txt_field2">
+                        <input type="password" id="lname" name="lname" required/> 
+                        <span></span>
+                        <label for="lname">Password:</label>
+                    </div>
+                    <button type="submit"  onClick={check_sign_in}>submit</button>
                 </form>
             </div>
         </>
