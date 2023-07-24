fc594a1f16f6c3ac27ba43a65cee5e2dba621208
David Curca <curcadavid2@gmail.com>
2023-07-24 15:21:44 +0300

    1+1 = 2

--- api/db.js
+++ api/db.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql');
+require('dotenv').config()
 
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'secret',
+    password: process.env.db_password,
     database: 'hof'
 });
 connection.connect()
--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
+const connection = require('../db')
 
 router.get("/test", (req, res) => {
-    res.json({message: 'working fine & dandy'})
+    connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].solution);
+        res.json({'1+1 = ': results[0].solution})
+    });
 })
 
 module.exports = router
