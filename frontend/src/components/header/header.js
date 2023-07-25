5c5feacc8ddd736006db45497299c49b5d063e20
David Curca <curcadavid2@gmail.com>
2023-07-25 15:15:20 +0300

    svg romania in header

--- frontend/src/components/header/header.js
+++ frontend/src/components/header/header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Logo from './romania.svg';
+// import Logo from './romania.svg';
 import './header.scss';
 export default function Header(props) {
     return (
         <>
             <div className="header--container">
-                <img className="header--logo" src={Logo}></img>
+                <img className="header--logo" src="romania.svg"></img>
                 <h1 className="header--title">Romanian Informatics Hall of Fame</h1>
             </div>
         </>
