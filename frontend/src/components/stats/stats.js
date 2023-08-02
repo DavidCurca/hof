a5ab2f5632aebbb0f7df564e542bddad3cd225a7
codeusser <vladnistor843@gmail.com>
2023-08-03 00:03:02 +0300

    O idee pentru pagina de stats

--- frontend/src/components/stats/stats.js
+++ frontend/src/components/stats/stats.js
@@ -1,10 +1,43 @@
 import './stats.scss'
 
 
+function ContestCard(props) {
+    return (
+        <>
+            <div className="contestCard">
+                <h2>{props.contestName}</h2>
+                <p>{props.contestDate}</p>
+                <p>{props.contestCategory}</p>
+            </div>
+        </>
+    )
+}
+
+function AddContestCard(props) {
+    return(
+        <div className="roundButton">
+            <h1>
+                +
+            </h1>
+        </div>
+    )
+}
+
 export default function Stats(){
     return (
         <>
-            Stats
+            <div className="Stats--page">
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+            <AddContestCard></AddContestCard>
+            </div>
         </>
     )
 }
