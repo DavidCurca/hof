6251028616636650930b8d46d99fe00b7185f1de
codeusser <vladnistor843@gmail.com>
2023-08-03 01:06:51 +0300

    putine modificari la stats

---
+++ frontend/src/components/contest/contest.js
@@ -0,0 +1,11 @@
+import 'contest.scss';
+
+
+export default function ContestPage(props) {
+    return (
+        <>
+            <h1>{props.ContestTitle}</h1>
+            
+        </>
+    )
+}
--- frontend/src/components/stats/stats.js
+++ frontend/src/components/stats/stats.js
@@ -1,10 +1,16 @@
+import { redirect } from 'react-router-dom'
 import './stats.scss'
-
+import { Link, useNavigate } from 'react-router-dom';
 
 function ContestCard(props) {
+    const navigate = useNavigate();
+    function redirectToContestPage(){
+        navigate('/stats/contest?id='+ props.contestId);
+    }
+
     return (
         <>
-            <div className="contestCard">
+            <div className="contestCard" onClick={redirectToContestPage}>
                 <h2>{props.contestName}</h2>
                 <p>{props.contestDate}</p>
                 <p>{props.contestCategory}</p>
@@ -24,10 +30,18 @@ function AddContestCard(props) {
 }
 
 export default function Stats(){
+
+    function redirectToContestPage(){
+        redirect("/stats/contest");
+    }
+
     return (
         <>
+            <h1 className='smt'>Concursuri Nationale</h1>
             <div className="Stats--page">
-            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
+        
+            
+            <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori" ></ContestCard>
             <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
             <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
             <ContestCard contestName="Baraj ONI" contestDate="2023" contestCategory="juniori"></ContestCard>
