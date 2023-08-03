42e10365275a117d9b6e15b864de22bff3effff4
David Curca <curcadavid2@gmail.com>
2023-08-03 21:33:12 +0300

    results backend & frontend

--- api/routes/result.js
+++ api/routes/result.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const {session_exists} = require('./session')
 const connection = require('../db');
 
+medalLookup = ["", "Bronz", "Argint", "Aur"]
+
 router.get("/addresult", (req, res) => {
-    res.json({message: 'result added succesfuly'})
     sessionId = req.query.session;
     personId = req.query.person;
     contestId = req.query.contest;
@@ -12,7 +13,19 @@ router.get("/addresult", (req, res) => {
     year = req.query.year;
     medal = req.query.medal;
 
-    
+    if(sessionId == undefined || personId == undefined || contestId == undefined || contestId == undefined ||
+       place == undefined || year == undefined || medal == undefined){
+        res.json({status: false, message: 'one or more params are undefined'})
+        return ;
+    }
+
+    if(!session_exists(sessionId)){
+        res.json({status: false, message: 'invalid session'})
+        return ;
+    }
+    medal = medalLookup[medal];
+
+    /// INSERT INTO results (contest, person, place, medal, year) VALUES (contestId, personId, ...)
 
 })
 
