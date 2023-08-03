1d1e3e175b4af967068adfd3ba96f350c60827d7
David Curca <curcadavid2@gmail.com>
2023-08-04 01:28:50 +0300

    api lists all contets, results almost done

--- api/routes/contest.js
+++ api/routes/contest.js
@@ -5,10 +5,24 @@ const connection = require('../db');
 async function getCategoryId(contestId){
     let ans = undefined;
     await new Promise((res, rej) => {
-        connection.query('SELECT (category_id) FROM contest WHERE id = ?', [contestId], function (err, results) {
+        connection.query('SELECT category_id FROM contest WHERE id = ?', [contestId], function (err, results) {
             if (err) throw err;
             if(results[0] != undefined){
                 ans = results[0].category_id;
+            }
+            res(0);
+        });
+    });
+    return ans;
+}
+
+async function getContests(){
+    let ans = [];
+    await new Promise((res, rej) => {
+        connection.query('SELECT id, name FROM contest', [], function (err, results) {
+            if (err) throw err;
+            if(results[0] != undefined){
+                ans.push({id: results[0].id, name: results[0].name})
             }
             res(0);
         });
@@ -26,6 +40,10 @@ router.get("/getid", async (req, res) => {
         return ;
     }
     res.json({status: true, message: category})
+})
+
+router.get("/getcontests", async (req, res) => {
+    res.json({status: true, message: await getContests()})
 })
 
 module.exports = {router, getCategoryId};
